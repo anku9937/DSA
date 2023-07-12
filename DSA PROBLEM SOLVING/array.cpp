0330ec01dca04eea326aae86a7b5c080542f1e2a
anku9937 <134857833+anku9937@users.noreply.github.com>
2023-07-11 23:06:44 -0700

    Add files via upload

---
+++ DSA PROBLEM SOLVING/array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number[15];
+    cout<<"Value at index14 "<<number[14]<<endl;
+        cout<<"Value at index14 "<<number[20]<<endl;
+    cout<<"Everyhting is fine"<<endl<<endl;
+
+    int second[3]={5,7,11};
+     cout<<"Value at second "<<second[2]<<endl;
+        cout<<"Value at index14 "<<second[1]<<endl;
+    cout<<"Everyhting is fine"<<endl<<endl;
+
+
+    // int third[15]={2,7};
+    // int n=15;
+    // for(int i=0;i<n;i++){
+    //     cout<<third[i]<<" "<<endl;
+    // }
+    // cout<<third[1]<<" ";
+
+
+
+    //   int fourth[10]={0};
+    // int n=10;
+    // for(int i=0;i<n;i++){
+    //     cout<<fourth[i]<<" ";
+    // }
+    
+      int fifth[10]={1};
+    int n=10;
+    for(int i=0;i<n;i++){
+        cout<<fifth[i]<<" ";
+    }
+
+}
